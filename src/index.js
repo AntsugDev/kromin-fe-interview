8dfac9946ef9ed93157ccf2ade48ed936f522603
Antonio Sugamele <antonio.sugamele@gmail.com>
2023-10-02 10:13:04 +0200

    first release

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { default as Login } from './pages/login/Login'
+export { default as Signup } from './pages/signup/Signup'
+export { default as Home } from './pages/home/Home'
+export { default as NotFound } from './pages/page-not-found/PageNotFound'
+export { default as History } from './pages/history/History'
